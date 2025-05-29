97da9072644e0fb0400960943fc1576d3697fbd8
golfer69 <spacemanenzo@gmail.com>
2025-05-29 13:10:07 +0800

    added top info

--- GenericRobot.cpp
+++ GenericRobot.cpp
@@ -1,3 +1,16 @@
+/**********|**********|**********|
+Program: GenericRobot.cpp  
+Course: Data Structures and Algorithms
+Trimester: 2510
+Name: Alif Akmal Bin Abdul Halim || Brian Ng Zheng Yang || Meor Hazimi Bin Meor Mohammad Fared || Yen Ming Jun
+ID: 242UC244L7 || 242UC244RD || 242UC244PU || 243UC246NQ
+Lecture Section: TC3L
+Tutorial Section: T11L
+Email: alif.akmal.abdul@student.mmu.edu.my || brian.ng.zheng@student.mmu.edu.my || meor.hazimi.meor@student.mmu.edu.my || yen.ming.jun@student.mmu.edu.my
+Phone: +60 18-355-5944|| +60 17-779 3199 || +60 19-752 1755 ||+60 11-5372 6266
+**********|**********|**********/
+
+
 #include "GenericRobot.h"
 #include <cstdlib>
 
--- Robot.h
+++ Robot.h
@@ -1,7 +1,19 @@
+/**********|**********|**********|
+Program: Robot.h 
+Course: Data Structures and Algorithms
+Trimester: 2510
+Name: Alif Akmal Bin Abdul Halim || Brian Ng Zheng Yang || Meor Hazimi Bin Meor Mohammad Fared || Yen Ming Jun
+ID: 242UC244L7 || 242UC244RD || 242UC244PU || 243UC246NQ
+Lecture Section: TC3L
+Tutorial Section: T11L
+Email: alif.akmal.abdul@student.mmu.edu.my || brian.ng.zheng@student.mmu.edu.my || meor.hazimi.meor@student.mmu.edu.my || yen.ming.jun@student.mmu.edu.my
+Phone: +60 18-355-5944|| +60 17-779 3199 || +60 19-752 1755 ||+60 11-5372 6266
+**********|**********|**********/
+
 #ifndef ROBOT_H
 #define ROBOT_H
 
-#include <string>
+#include <string> 
 #include <vector>
 
 class Robot {
