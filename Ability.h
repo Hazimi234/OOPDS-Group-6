236e37599f2e67ef985be3bdf6fdc3e33910acb9
Hazimi234 <meorhazimi05@gmail.com>
2025-06-01 17:06:45 +0800

    smol tets

--- Ability.h
+++ Ability.h
@@ -35,34 +35,9 @@ public:
     virtual bool isTrackBot() const { return false; }
     virtual bool isHideBot() const { return false; }
     virtual bool isJumpBot() const { return false; }
-    virtual bool hasUses() const { return false; } // Default implementation
-
-    virtual bool isShootingAbility() const { return false; }
-    virtual bool isMovingAbility() const { return false; }
-    virtual bool isSeeingAbility() const { return false; }
+    virtual bool hasUses() const { return false; } 
 
     virtual ~Ability() = default;
-};
-
-// --- Shooting Ability Base ---
-class ShootingAbility : public Ability
-{
-public:
-    bool isShootingAbility() const override { return true; }
-};
-
-// --- Moving Ability Base ---
-class MovingAbility : public Ability
-{
-public:
-    bool isMovingAbility() const override { return true; }
-};
-
-// --- Seeing Ability Base ---
-class SeeingAbility : public Ability
-{
-public:
-    bool isSeeingAbility() const override { return true; }
 };
 
 #endif
